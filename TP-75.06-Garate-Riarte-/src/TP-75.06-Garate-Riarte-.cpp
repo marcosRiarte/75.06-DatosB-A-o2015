782bce7a70d3935922d638596838f9174094768b
marcosRiarte <mriarte@fi.uba.ar>
2015-08-14 19:37:04 -0300

    Probando nuevo testeoo

---
+++ TP-75.06-Garate-Riarte-/src/TP-75.06-Garate-Riarte-.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : 06-Garate-Riarte-.cpp
+// Author      : Marcos Riarte
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+
+
+int main() {
+	std::cout << "!!!Probando, MARCOS (AGREGAR NOMBRE DE COMITER!!!" << std::endl; // prints !!!Hello World!!!
+	return 0;
+}
